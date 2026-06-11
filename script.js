7de700f5e1f04d0fe8802b04cbb0373667428ce2
henriquelemosfelipe-oss <henrique.lemos.felipe@escola.pr.gov.br>
2026-06-11 07:43:17 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const botao = document.getElementById("mensagemBtn");
+const mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", () => {
+
+    mensagem.innerHTML =
+    "🌱 Cada árvore plantada, cada gota de água economizada e cada atitude consciente ajudam a construir um futuro melhor para todos!";
+});
